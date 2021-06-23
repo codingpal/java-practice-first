6d18cb0ca238b7e08e3b1ac8f7f96694af5fe6c2
codingpal <amrutagtask@gmail.com>
2021-06-23 21:00:18 +0530

    Testing Eclipse Git Repository

--- Nikhil/callfunction.java
+++ Nikhil/callfunction.java
@@ -6,6 +6,7 @@ public class callfunction {
 	public static void function1 () {
 		
 		System.out.println("Good Morning");
+		System.out.println("Just for test");
 	}	
 	
 	public static void main(String[] args) {
